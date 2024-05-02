2a58b90c278a39f4a9c3ba01bffc5a5e8b3ddbf0
brayanospina2005 <ospinabrayan431@gmail.com>
2024-05-01 23:01:44 -0500

    swiper

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,49 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+/* remove menu */
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* swipper */
+
+const swiperHome = new Swiper('.home__swiper', {
+
+    loop: true,
+    speed: 800,
+    parallax: true,
+    effect: 'fade',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+  
+ 
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      formatFractionCurrent: (Number) => { return '0' + Number},
+      formatFractionTotal: (Number) => { return '0' + Number},
+    },
+  
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
